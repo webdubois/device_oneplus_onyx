aaf09deee9c9a354d1326766cfaa924ce87d4665
YumeMichi <do4suki@gmail.com>
2016-12-22 11:55:58 +0800

    onyx: Update RIL variant

--- init/init_onyx.cpp
+++ init/init_onyx.cpp
@@ -46,7 +46,8 @@ void vendor_load_properties()
         /* China */
         property_set("ro.product.model", "ONE E1001");
         property_set("ro.rf_version", "TDD_FDD_Ch_All");
-        property_set("ro.telephony.default_network", "20,20");
+        property_set("telephony.lteOnCdmaDevice", "1");
+        property_set("ro.telephony.default_network", "22,22");
     } else if (rf_version == "102") {
         /* Asia/Europe */
         property_set("ro.product.model", "ONE E1003");
@@ -56,12 +57,14 @@ void vendor_load_properties()
         /* America */
         property_set("ro.product.model", "ONE E1005");
         property_set("ro.rf_version", "TDD_FDD_Am");
+        property_set("telephony.lteOnCdmaDevice", "1");
         property_set("ro.telephony.default_network", "9,9");
     } else if (rf_version == "107"){
         /* China CTCC Version */
         property_set("ro.product.model", "ONE E1000");
         property_set("ro.rf_version", "TDD_FDD_ALL_OPTR");
-        property_set("ro.telephony.default_network", "20,20");
+        property_set("telephony.lteOnCdmaDevice", "1");
+        property_set("ro.telephony.default_network", "22,22");
     }
     device = property_get("ro.product.device");
     INFO("Found rf_version : %s setting build properties for %s device\n", rf_version.c_str(), device.c_str());
