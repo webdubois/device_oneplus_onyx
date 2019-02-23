a77729b2eb15e9fc3a99f1c5b0a7d00d55352f4c
YumeMichi <do4suki@gmail.com>
2019-02-23 15:05:50 +0800

    onyx: Build thermal HIDL HAL
     * Log spam is gone:
       E HardwarePropertiesManagerService-JNI: Unable to get Thermal service.
       E HardwarePropertiesManagerService-JNI: Couldn't get device temperatures because of HAL error.
       W hwservicemanager: getTransport: Cannot find entry android.hardware.thermal@1.0::IThermal/default in either framework or device manifest.
    
    Change-Id: Ie8a04b58f7357086f42ddfd5cfe4a9fc555dfee0
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- device.mk
+++ device.mk
@@ -265,6 +265,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
 # Thermal config
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-8974.conf:system/vendor/etc/thermal-engine-8974.conf
